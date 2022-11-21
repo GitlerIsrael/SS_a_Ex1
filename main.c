385e35f87f990e57f688beb59768cf10ecf10713
GitlerIsrael <99247120+GitlerIsrael@users.noreply.github.com>
2022-11-21 16:00:38 +0200

    wip

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ int main(){
     printf("The Armstrong numbers are:");
     for (int i = x1; i <= x2 ; i++) {
         if(isArmstrong(i)) {
-            printf(" %d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
@@ -18,7 +18,7 @@ int main(){
     printf("The Palindromes are:");
     for (int i = x1; i <= x2 ; i++) {
         if(isPalindrome(i)) {
-            printf(" %d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
@@ -26,7 +26,7 @@ int main(){
     printf("The Prime numbers are:");
     for (int i = x1; i<= x2 ; i++) {
         if(isPrime(i)) {
-            printf(" %d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
@@ -34,7 +34,7 @@ int main(){
     printf("The Strong numbers are:");
     for (int i = x1; i <= x2 ; i++) {
         if(isStrong(i)) {
-            printf(" %d ", i);
+            printf(" %d", i);
         }
     }
     printf("\n");
